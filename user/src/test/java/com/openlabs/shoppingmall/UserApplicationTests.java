0988c8f9eb279c3c917f1257f18b95a2468bc59f
Yangyunhui454545 <sts05137@naver.com>
2023-08-17 11:14:10 +0900

    inital commit

---
+++ user/src/test/java/com/openlabs/shoppingmall/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openlabs.shoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
