2b6a454c656441531c3596208421eff2ee6adfcb
Yangyunhui454545 <sts05137@naver.com>
2023-08-30 16:29:29 +0900

    swagger 설정 추가

---
+++ framework/src/main/java/com/openlabs/framework/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.openlabs.framework.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration    // 스프링 실행시 설정파일 어노테이션
+@EnableSwagger2    // Swagger2를 사용하겠다는 어노테이션
+public class SwaggerConfig{
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("ShoppingMall")
+                .description("shopping mall Example")
+                .build();
+    }
+
+    //swagger 설정.
+    @Bean
+    public Docket getDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.openlabs.shoppingmall"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
--- user/src/main/java/com/openlabs/shoppingmall/controller/SampleController.java
+++ user/src/main/java/com/openlabs/shoppingmall/controller/SampleController.java
@@ -1,17 +1,22 @@
 package com.openlabs.shoppingmall.controller;
 
 import com.openlabs.shoppingmall.service.SampleService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@Api(tags = {"sample test"})
 @RestController
 @RequiredArgsConstructor
 public class SampleController {
     private final SampleService service;
 
     @GetMapping("/test")
+    @ApiOperation(value = "test api")
     public String test(@RequestParam String param) {
         return service.sampleService(param);
     }
