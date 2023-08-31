b01a82cb3469eb62f3780611af3f131e027bbff4
Yangyunhui454545 <sts05137@naver.com>
2023-08-31 16:57:45 +0900

    swagger 설정 api 모듈로 이동

---
+++ user/src/main/java/com/openlabs/shoppingmall/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.openlabs.shoppingmall.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration    // 스프링 실행시 설정파일 어노테이션
+@EnableSwagger2    // Swagger2를 사용하겠다는 어노테이션
+public class SwaggerConfig {
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("ShoppingMall")
+                .version("0.1")
+                .description("shopping mall Example")
+                .build();
+    }
+
+    //swagger 설정.
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.openlabs.shoppingmall.controller"))
+                .paths(PathSelectors.ant("/labshop/v1/**"))
+                .build();
+    }
+}
