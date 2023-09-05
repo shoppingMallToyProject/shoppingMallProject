985a6fab40837f5af01f8ee1c06af746ee69e414
Yangyunhui454545 <sts05137@naver.com>
2023-09-05 11:07:30 +0900

    Object convert 할 수 잇는 util성 메소드 추가

---
+++ framework/src/main/java/com/openlabs/framework/util/ObjectConverter.java
@@ -0,0 +1,26 @@
+package com.openlabs.framework.util;
+
+import com.openlabs.framework.config.AppConfig;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ObjectConverter {
+
+
+    private ObjectConverter() {
+    }
+
+    private static ModelMapper getModelMapper() {
+        ApplicationContext ac
+                = new AnnotationConfigApplicationContext(AppConfig.class);
+        return ac.getBean(ModelMapper.class);
+    }
+
+    public static <S, T> T toObject(S source, Class<T> targetClass) {
+        ModelMapper mapper = getModelMapper();
+        return mapper.map(source, targetClass);
+    }
+}
